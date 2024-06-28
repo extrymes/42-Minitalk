5c06bebb149f66c6de6a1f11417843890f6e7e6b
extrymes <simon.abras@gmail.com>
2024-06-28 19:22:55 +0200

    feat(client): add `ft_send_signal()` function

--- client.c
+++ client.c
@@ -6,16 +6,13 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 21:59:19 by sabras            #+#    #+#             */
-/*   Updated: 2024/06/28 11:51:31 by sabras           ###   ########.fr       */
+/*   Updated: 2024/06/28 15:53:21 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_send_signal(int pid, char c)
-{
-	
-}
+static void	ft_send_signal(int pid, char c);
 
 int	main(int ac, char **av)
 {
@@ -31,4 +28,20 @@ int	main(int ac, char **av)
 	while (msg[i])
 		ft_send_signal(pid, msg[i++]);
 	return (0);
+}
+
+static void	ft_send_signal(int pid, char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (0x01 << bit)) != 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		bit++;
+	}
 }
