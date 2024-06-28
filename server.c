3175099da330cea7eade1c716a5b8cdca2227222
extrymes <simon.abras@gmail.com>
2024-06-28 19:23:42 +0200

    feat(server): add `ft_handle_server()` function

--- server.c
+++ server.c
@@ -6,16 +6,13 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 21:59:17 by sabras            #+#    #+#             */
-/*   Updated: 2024/06/28 11:51:13 by sabras           ###   ########.fr       */
+/*   Updated: 2024/06/28 15:53:17 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_handle_server(int signal)
-{
-	
-}
+void	ft_handle_server(int signal);
 
 int	main(void)
 {
@@ -28,4 +25,20 @@ int	main(void)
 		pause();
 	}
 	return (0);
+}
+
+void	ft_handle_server(int signal)
+{
+	static int	bit = 0;
+	static int	i = 0;
+
+	if (signal == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
 }
