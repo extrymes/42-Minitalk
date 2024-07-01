9d0df32f4e7c3c1670fcc161412e3b30a89b4884
extrymes <simon.abras@gmail.com>
2024-07-02 00:17:20 +0200

    feat: use sigaction to handle signals and return response to client

--- client.c
+++ client.c
@@ -6,42 +6,75 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 21:59:19 by sabras            #+#    #+#             */
-/*   Updated: 2024/06/28 15:53:21 by sabras           ###   ########.fr       */
+/*   Updated: 2024/07/01 23:41:50 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	ft_send_signal(int pid, char c);
+static int	g_response;
 
-int	main(int ac, char **av)
+void	ft_sig_handler(int signum, siginfo_t *info, void *context)
 {
-	int		pid;
-	char	*msg;
-	int		i;
+	static int	i;
 
-	if (ac != 3)
-		return (ft_printf(RED "Usage:\t./client [SERVER_PID] [MESSAGE]\n" RESET), 1);
-	pid = ft_atoi(av[1]);
-	msg = av[2];
-	i = 0;
-	while (msg[i])
-		ft_send_signal(pid, msg[i++]);
+	(void)context;
+	(void)info;
+	g_response = 1;
+	if (signum == SIGUSR2)
+		i++;
+	else if (signum == SIGUSR1)
+		ft_printf("Bytes received: \033[32m%d\033[0m\n", i / 8);
+}
+
+int	ft_char_to_bin(char c, int srv_pid)
+{
+	int	itr;
+	int	bit_idx;
+
+	bit_idx = 7;
+	while (bit_idx >= 0)
+	{
+		if ((c >> bit_idx) & 1)
+			kill(srv_pid, SIGUSR1);
+		else
+			kill(srv_pid, SIGUSR2);
+		itr = 0;
+		while (g_response == 0)
+		{
+			if (itr == 50)
+			{
+				ft_printf(RED "No response from server" RESET, 1);
+				exit(1);
+			}
+			itr++;
+			usleep(100);
+		}
+		g_response = 0;
+		bit_idx--;
+	}
 	return (0);
 }
 
-static void	ft_send_signal(int pid, char c)
+int	main(int ac, char **av)
 {
-	int	bit;
+	struct sigaction	sa;
+	int					byte_idx;
+	int					srv_pid;
 
-	bit = 0;
-	while (bit < 8)
-	{
-		if ((c & (0x01 << bit)) != 0)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(100);
-		bit++;
-	}
+	if (ac != 3)
+		return (ft_printf(RED "Usage:\t./client [SRV_PID] [MESSAGE]\n" RESET), 1);
+	byte_idx = 0;
+	srv_pid = ft_atoi(av[1]);
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+	sa.sa_sigaction = ft_sig_handler;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		ft_printf(RED "Sigaction error\n" RESET, 1);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		ft_printf(RED "Sigaction error\n" RESET, 1);
+	while (av[2][byte_idx])
+		ft_char_to_bin(av[2][byte_idx++], srv_pid);
+	ft_char_to_bin('\0', srv_pid);
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,39 +6,58 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 21:59:17 by sabras            #+#    #+#             */
-/*   Updated: 2024/06/29 00:40:29 by sabras           ###   ########.fr       */
+/*   Updated: 2024/07/01 23:31:46 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void	ft_handle_server(int signal);
+void	ft_bin_to_char(int signum, char *c)
+{
+	if (signum == SIGUSR1)
+		*c = (*c << 1) | 1;
+	else if (signum == SIGUSR2)
+		*c <<= 1;
+}
+
+void	ft_sig_handler(int signum, siginfo_t *info, void *context)
+{
+	static int		client_pid;
+	static char		c;
+	static int		i;
+
+	(void)context;
+	if (client_pid == 0)
+		client_pid = info->si_pid;
+	ft_bin_to_char(signum, &c);
+	if (++i == 8)
+	{
+		i = 0;
+		if (!c)
+		{
+			kill(client_pid, SIGUSR1);
+			client_pid = 0;
+			return ;
+		}
+		ft_printf("%c", c);
+		c = 0;
+	}
+	kill(client_pid, SIGUSR2);
+}
 
 int	main(void)
 {
-	ft_printf("My Server PID is \x1b[32m%d\x1b[0m\n", getpid());
-	ft_printf(YELLOW "Waiting for a message...\n" RESET);
+	struct sigaction	sa;
+
+	ft_printf("My Server PID: \033[32m%d\033[0m\n", getpid());
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+	sa.sa_sigaction = ft_sig_handler;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		ft_printf(RED "Error sigaction\n" RESET, 1);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		ft_printf(RED "Error sigaction\n" RESET, 1);
 	while (1)
-	{
-		signal(SIGUSR1, ft_handle_server);
-		signal(SIGUSR2, ft_handle_server);
 		pause();
-	}
 	return (0);
-}
-
-static void	ft_handle_server(int signal)
-{
-	static int	bit = 0;
-	static int	i = 0;
-
-	if (signal == SIGUSR1)
-		i |= (0x01 << bit);
-	bit++;
-	if (bit == 8)
-	{
-		ft_printf("%c", i);
-		bit = 0;
-		i = 0;
-	}
 }
