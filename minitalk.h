10624f006f3611a16a254862d5f241b8763b1013
extrymes <simon.abras@gmail.com>
2024-07-01 20:55:03 +0200

    chore: update minitalk.h

--- minitalk.h
+++ minitalk.h
@@ -6,13 +6,13 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 02:04:53 by sabras            #+#    #+#             */
-/*   Updated: 2024/06/28 02:05:54 by sabras           ###   ########.fr       */
+/*   Updated: 2024/07/01 20:27:10 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 # include "libft/libft.h"
-# include "libft/ft_printf/ft_printf.h"
+# include <stdlib.h>
 # include <signal.h>
 #endif
