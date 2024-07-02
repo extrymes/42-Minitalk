f9b3c7ec7ed86c29301896d6dd1fa64549048b3c
extrymes <simon.abras@gmail.com>
2024-07-02 22:15:06 +0200

    refactor(client): clean code

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 21:59:19 by sabras            #+#    #+#             */
-/*   Updated: 2024/07/01 23:41:50 by sabras           ###   ########.fr       */
+/*   Updated: 2024/07/02 22:14:45 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ void	ft_sig_handler(int signum, siginfo_t *info, void *context)
 	(void)context;
 	(void)info;
 	g_response = 1;
-	if (signum == SIGUSR2)
-		i++;
-	else if (signum == SIGUSR1)
+	if (signum == SIGUSR1)
 		ft_printf("Bytes received: \033[32m%d\033[0m\n", i / 8);
+	else if (signum == SIGUSR2)
+		i++;
 }
 
 int	ft_char_to_bin(char c, int srv_pid)
@@ -59,12 +59,11 @@ int	ft_char_to_bin(char c, int srv_pid)
 int	main(int ac, char **av)
 {
 	struct sigaction	sa;
-	int					byte_idx;
 	int					srv_pid;
+	int					i;
 
 	if (ac != 3)
 		return (ft_printf(RED "Usage:\t./client [SRV_PID] [MESSAGE]\n" RESET), 1);
-	byte_idx = 0;
 	srv_pid = ft_atoi(av[1]);
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART | SA_SIGINFO;
@@ -73,8 +72,9 @@ int	main(int ac, char **av)
 		ft_printf(RED "Sigaction error\n" RESET, 1);
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
 		ft_printf(RED "Sigaction error\n" RESET, 1);
-	while (av[2][byte_idx])
-		ft_char_to_bin(av[2][byte_idx++], srv_pid);
+	i = 0;
+	while (av[2][i])
+		ft_char_to_bin(av[2][i++], srv_pid);
 	ft_char_to_bin('\0', srv_pid);
 	return (0);
 }
